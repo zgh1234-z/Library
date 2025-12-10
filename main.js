b18c08c28337b961b781341df30c42f487443e75
zgh <ghahremans908@gmil.com>
2025-12-10 12:55:29 +0330

    add the array

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const Nid = Number(prompt("شماره کتاب جدید"));
 const NTitle = prompt("نام کتاب جدید را وارد کن ");
-const NYear = prompt("سال چاپ کتاب را وارد کن ");
+const NYear = Number(prompt("سال چاپ کتاب را وارد کن "));
 const NAuthor = prompt("نام نویسنده را وارد کن ");
 
 function Book(id, Title, Year, Author) {
@@ -14,7 +14,10 @@ const book1 = new Book(1, " ریاضی", 1402, "علی");
 const book2 = new Book(2, " فیزیک", 1404, "رضا");
 const book3 = new Book(3, " شیمی", 1401, "سارا");
 const Nbook = new Book(Nid, NTitle, NYear, NAuthor);
-Book.push(Nbook);
+
+let Books = [book1, book2, book3];
+Books.push(Nbook);
+
 document.getElementById("p").innerHTML = book1.N;
 
-console.log(Book());
+console.log(Books);
