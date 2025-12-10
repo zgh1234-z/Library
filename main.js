9c5decaaba26a2e13e3d710c29203ed47a911c7e
zgh <ghahremans908@gmil.com>
2025-12-10 10:18:55 +0330

    add the Add product via user

--- main.js
+++ main.js
@@ -1,14 +1,20 @@
-let id = Symbol("id");
+const Nid = Number(prompt("شماره کتاب جدید"));
+const NTitle = prompt("نام کتاب جدید را وارد کن ");
+const NYear = prompt("سال چاپ کتاب را وارد کن ");
+const NAuthor = prompt("نام نویسنده را وارد کن ");
 
 function Book(id, Title, Year, Author) {
-  [id] = this.id;
-  Title = this.Title;
-  Year = this.Year;
-  Author = this.Author;
+  this.id = id;
+  this.Title = Title;
+  this.Year = Year;
+  this.Author = Author;
 }
 
-const book1 = new Book(101, ریاضی, 1402, ali);
-const book2 = new Book(102, فیزیک, 1404, reza);
-const book3 = new Book(103, شیمی, 1401, sara);
+const book1 = new Book(1, " ریاضی", 1402, "علی");
+const book2 = new Book(2, " فیزیک", 1404, "رضا");
+const book3 = new Book(3, " شیمی", 1401, "سارا");
+const Nbook = new Book(Nid, NTitle, NYear, NAuthor);
+Book.push(Nbook);
+document.getElementById("p").innerHTML = book1.N;
 
-document.getElementById("p").innerHTML = book1;
+console.log(Book());
